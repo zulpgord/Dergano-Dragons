61a485ece93ac64a6fb5c5363f7fc2fc466a8de8
zulpgord <sicurezza.cenci@gmail.com>
2026-09-21 20:17:20 +0200

    Add files via upload

--- backend/src/routes/groupRoutes.js
+++ backend/src/routes/groupRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getGroups, createGroup, deleteGroup, setGroupMembers } = require('../controllers/groupController');
+const { getGroups, createGroup, deleteGroup, setGroupMembers, sendGroupEmail } = require('../controllers/groupController');
 const { authMiddleware, adminOnly } = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/', authMiddleware, adminOnly, getGroups);
 router.post('/', authMiddleware, adminOnly, createGroup);
 router.delete('/:id', authMiddleware, adminOnly, deleteGroup);
 router.put('/:id/members', authMiddleware, adminOnly, setGroupMembers);
+router.post('/:id/email', authMiddleware, adminOnly, sendGroupEmail);
 
 module.exports = router;
